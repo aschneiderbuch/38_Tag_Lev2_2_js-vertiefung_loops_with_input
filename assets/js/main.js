8c20fbc111f96021ac51e448012f20f25834712d
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-03 18:58:28 +0100

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,30 @@ Function mit der am Ende Wort Loop ausgeben
         loop 
             push()
         array in String
-        
+
 
 */
+
+
+let button = document.querySelector("#button");
+
+button.addEventListener("click", (event) => {
+event.preventDefault();
+
+let array = ["L","o","o","p"];
+console.log(array);
+console.log(array.length);
+
+let input = document.querySelector("#input").value;
+console.log(input);
+
+array.splice(1,0,`${input}`)
+console.log(array);
+
+let arrayString = String(array.join(""));
+console.log(arrayString);
+
+let output = document.querySelector("#output");
+output.innerHTML = arrayString;
+});
+
