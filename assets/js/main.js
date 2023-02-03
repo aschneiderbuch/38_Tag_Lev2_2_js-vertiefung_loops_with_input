43cb40c4cf9c5f88294d4c05940bbf150e5756ca
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-03 18:41:07 +0100

    htmlGerüst

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+console.log("test");
+
+/* 
+Frage
+
+Inputfeld für Zahl
+
+Function mit der am Ende Wort Loop ausgeben
+        die o o o o o  varrieren durch User eingabe
+
+
+        array = 
+        loop 
+            push()
+        array in String
+        
+
+*/
