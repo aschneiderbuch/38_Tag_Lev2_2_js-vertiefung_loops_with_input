d4fe5aaa1fe92058d8fbbdea926cf2ae584498e6
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-03 19:10:47 +0100

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,30 +26,27 @@ button.addEventListener("click", (event) => {
 
     // array def und init und befüllen
 let array = ["L","o","o","p"];
-console.log(array);
-console.log(array.length);
+//console.log(array);
+//console.log(array.length);
 
 // input holen
 let input = document.querySelector("#input").value;
-console.log(input);
+//console.log(input);
 
 // loop
 for (rundenIndex = 0; rundenIndex < input.length; rundenIndex = rundenIndex + 1) {
-    console.log("Runden Index: " + rundenIndex);
+   // console.log("Runden Index: " + rundenIndex);
 
     // input ins array hinzufügen
 array.splice(1,0,`${input[rundenIndex]}`)
-console.log(array);
+//console.log(array);
 
 }
 
 
-
-
-
 // für output Kommas weg machen
 let arrayString = String(array.join(""));
-console.log(arrayString);
+//console.log(arrayString);
 
 // output in html 
 let output = document.querySelector("#output");
